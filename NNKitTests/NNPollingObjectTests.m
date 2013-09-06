b569a4f774d9599a52e05d11f8e87c4b0e2e3061
Scott Perry <github@numist.net>
2013-09-06 00:25:38 -0700

    It's a fair point...

---
+++ NNKitTests/NNPollingObjectTests.m
@@ -0,0 +1,34 @@
+//
+//  NNPollingObjectTests.m
+//  NNKit
+//
+//  Created by Scott Perry on 09/06/13.
+//  Copyright (c) 2013 Scott Perry. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NNPollingObjectTests : XCTestCase
+
+@end
+
+@implementation NNPollingObjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"Gosh, how do you even test a polling object that takes *time*");
+}
+
+@end
