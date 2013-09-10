82f72b39a746bb5992174c631ae540a1941449c8
Scott Perry <github@numist.net>
2013-09-09 21:55:20 -0700

    Remove BailUnless from runtime. I should include Debugger in here at some point, maybe as a submodule? Need to do prefixes or something though :/

--- NNKit/API Extension/runtime.c
+++ NNKit/API Extension/runtime.c
@@ -13,49 +13,40 @@
 //
 
 #include "runtime.h"
+
 #include <string.h>
 #include <stdlib.h>
 
-// Lifted from https://github.com/numist/Debugger/blob/master/debugger.h
-#ifndef BailWithBlockUnless
-#define BailWithBlockUnless(exp,block) \
-do { \
-    if (!(exp)) { \
-        return block(); \
-    } \
-} while(0)
-#endif
 
 // Like the other class_copy* functions, the caller must free the return value of this function with free()
 objc_property_attribute_t *nn_property_copyAttributeList(objc_property_t property, unsigned int *outCount)
 {
-    void *(^failure)(void) = ^{
-        if (outCount) {
-            *outCount = 0;
-        }
-        return NULL;
-    };
+    if (outCount) {
+        *outCount = 0;
+    }
     
     // For more information about the property type string, see the Declared Properties section of the Objective-C Runtime Programming Guide
     const char *constAttributes = property_getAttributes(property);
-    BailWithBlockUnless(constAttributes, failure);
+    if (!constAttributes) {
+        return NULL;
+    }
     
     /**
      *  ┏━━━━━━━━┓
      *  ┃nameptr         ┃
      *  ┃valueptr        ┃
-     *  ┠────────────────┨
+     *  ┠────────┨
      *  ┃nameptr         ┃
      *  ┃valueptr        ┃
-     *  ┠────────────────┨
+     *  ┠────────┨
      *  ┃nameptr         ┃
      *  ┃valueptr        ┃
-     *  ┠────────────────┨
+     *  ┠────────┨
      *  ┃...             ┃
-     *  ┠────────────────┨
+     *  ┠────────┨
      *  ┃NULL            ┃
      *  ┃NULL            ┃
-     *  ┠────────────────┨
+     *  ┠────────┨
      *  ┃N0Valueue0N0Valu┃
      *  ┃e0N0Valueueueue0┃
      *  ┃...             ┃
@@ -74,7 +65,10 @@ objc_property_attribute_t *nn_property_copyAttributeList(objc_property_t propert
     size_t attributeListSize = (attributeCount + 1) * sizeof(objc_property_attribute_t); // The list of attributes, plus an extra attribute containing NULL for its name and value.
     size_t attributeStringsSize = (strlen(constAttributes) + attributeCount + 1) * sizeof(char); // The attribute names and values, plus the extra necessary NUL terminators.
     objc_property_attribute_t *attributeList = calloc(attributeListSize + attributeStringsSize, 1);
-    BailWithBlockUnless(attributeList, failure);
+    if (!attributeList) {
+        return NULL;
+    }
+    
     
     // Initialize the attribute string area.
     char *attributeStrings = (char *)attributeList + attributeListSize;
@@ -87,7 +81,7 @@ objc_property_attribute_t *nn_property_copyAttributeList(objc_property_t propert
         // Attribute pairs must contain a name!
         if (*name == '\0') {
             free(attributeList);
-            return failure();
+            return NULL;
         }
         
         // NUL-terminating the name requires first moving the rest of the string which requires some extra housekeeping because of strsep.
