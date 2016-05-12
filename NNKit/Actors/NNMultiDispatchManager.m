138f1396f067d3f15fa3271793fb04721432c2e4
Scott Perry <github@numist.net>
2016-05-11 17:38:57 -0700

    Fix concurrency issues in NNMultiDispatchManager

--- NNKit/Actors/NNMultiDispatchManager.m
+++ NNKit/Actors/NNMultiDispatchManager.m
@@ -43,91 +43,123 @@
     return self;
 }
 
+@synthesize enabled = _enabled;
+
+- (BOOL)isEnabled;
+{
+    @synchronized(self) {
+        return self->_enabled;
+    }
+}
+
+- (void)setIsEnabled:(BOOL)enabled;
+{
+    @synchronized(self) {
+        self->_enabled = enabled;
+    }
+}
+
 - (void)addObserver:(id)observer;
 {
-    NSAssert([NSThread isMainThread], @"Boundary call was not made on main thread");
     NSParameterAssert([observer conformsToProtocol:self.protocol]);
     
-    [self.observers addObject:observer];
+    @synchronized(self) {
+        [self.observers addObject:observer];
+    }
 }
 
 - (BOOL)hasObserver:(id)observer;
 {
     NSAssert([NSThread isMainThread], @"Boundary call was not made on main thread");
-
-    return [self.observers containsObject:observer];
+    
+    @synchronized(self) {
+        return [self.observers containsObject:observer];
+    }
 }
 
 - (void)removeObserver:(id)observer;
 {
     NSAssert([NSThread isMainThread], @"Boundary call was not made on main thread");
-
-    [self.observers removeObject:observer];
+    
+    @synchronized(self) {
+        [self.observers removeObject:observer];
+    }
 }
 
 - (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector;
 {
-    return [self.signatureCache objectForKey:NSStringFromSelector(aSelector)];
+    @synchronized(self) {
+        return [self.signatureCache objectForKey:NSStringFromSelector(aSelector)];
+    }
 }
 
 - (void)forwardInvocation:(NSInvocation *)anInvocation;
 {
-    if (self.enabled) {
-        if (anInvocation.methodSignature.isOneway) {
-            // If we're going async, copy the invocation to avoid multiple threads calling -invoke or otherwise acting in a thread-unsafe manner.
-            anInvocation = [anInvocation nn_copy];
-            [anInvocation retainArguments];
+    @synchronized(self) {
+        if (self.enabled) {
+            if (anInvocation.methodSignature.isOneway) {
+                // If we're going async, copy the invocation to avoid multiple threads calling -invoke or otherwise acting in a thread-unsafe manner.
+                anInvocation = [anInvocation nn_copy];
+                [anInvocation retainArguments];
+            }
+            
+            NSAssert(strstr(anInvocation.methodSignature.methodReturnType, "v"), @"Method return type must be void.");
+            dispatch_block_t dispatch = ^{
+                [self private_forwardInvocation:anInvocation];
+            };
+            if (anInvocation.methodSignature.isOneway) {
+                dispatch_async(dispatch_get_main_queue(), dispatch);
+            } else {
+                despatch_sync_main_reentrant(dispatch);
+            }
         }
         
-        NSAssert(strstr(anInvocation.methodSignature.methodReturnType, "v"), @"Method return type must be void.");
-        dispatch_block_t dispatch = ^{
-            BOOL required = YES;
-            BOOL instance = YES;
-            
-            BOOL sanity = nn_selector_belongsToProtocol(anInvocation.selector, self.protocol, &required, &instance);
-#           ifdef NS_BLOCK_ASSERTIONS
-                (void)sanity;
-#           else
-                NSAssert(sanity && instance, @"Selector %@ is not actually part of protocol %@?!", NSStringFromSelector(anInvocation.selector), NSStringFromProtocol(self.protocol));
-#           endif
-            
-            // This loops over .observers.allObjects to retain the members of the collection for the duration of the loop. Intentional mutations to the collection were being avoided, but object deallocation was unpredictably mutating the collection during fast enumeration.
-            // In fact, there should probably be a radar about using fast enumeration on weak collections…
-            for (id obj in self.observers.allObjects) {
-                if ([obj respondsToSelector:anInvocation.selector] || required) {
-                    [anInvocation invokeWithTarget:obj];
-                }
-            }
-        };
-        if (anInvocation.methodSignature.isOneway) {
-            dispatch_async(dispatch_get_main_queue(), dispatch);
-        } else {
-            despatch_sync_main_reentrant(dispatch);
-        }
+        anInvocation.target = nil;
+        [anInvocation invoke];
     }
-    
-    anInvocation.target = nil;
-    [anInvocation invoke];
 }
 
 #pragma mark Private
 
 - (void)_cacheMethodSignaturesForProcotol:(Protocol *)protocol;
 {
-    unsigned int totalCount;
-    for (uint8_t i = 0; i < 1 << 1; ++i) {
-        struct objc_method_description *methodDescriptions = nn_autofree(protocol_copyMethodDescriptionList(protocol, i & 1, YES, &totalCount));
+    @synchronized(self) {
+        unsigned int totalCount;
+        for (uint8_t i = 0; i < 1 << 1; ++i) {
+            struct objc_method_description *methodDescriptions = nn_autofree(protocol_copyMethodDescriptionList(protocol, i & 1, YES, &totalCount));
+            
+            for (unsigned j = 0; j < totalCount; j++) {
+                struct objc_method_description *methodDescription = methodDescriptions + j;
+                [self.signatureCache setObject:[NSMethodSignature signatureWithObjCTypes:methodDescription->types] forKey:NSStringFromSelector(methodDescription->name)];
+            }
+        }
         
+        // Recurse to include other protocols to which this protocol adopts
+        Protocol * __unsafe_unretained *adoptions = (Protocol * __unsafe_unretained *)nn_autofree(protocol_copyProtocolList(protocol, &totalCount));
         for (unsigned j = 0; j < totalCount; j++) {
-            struct objc_method_description *methodDescription = methodDescriptions + j;
-            [self.signatureCache setObject:[NSMethodSignature signatureWithObjCTypes:methodDescription->types] forKey:NSStringFromSelector(methodDescription->name)];
+            [self _cacheMethodSignaturesForProcotol:adoptions[j]];
         }
     }
-    
-    // Recurse to include other protocols to which this protocol adopts
-    Protocol * __unsafe_unretained *adoptions = (Protocol * __unsafe_unretained *)nn_autofree(protocol_copyProtocolList(protocol, &totalCount));
-    for (unsigned j = 0; j < totalCount; j++) {
-        [self _cacheMethodSignaturesForProcotol:adoptions[j]];
+}
+
+- (void)private_forwardInvocation:(NSInvocation *)anInvocation;
+{
+    @synchronized(self) {
+        BOOL required = YES;
+        BOOL instance = YES;
+        
+#ifndef NS_BLOCK_ASSERTIONS
+        BOOL sanity = nn_selector_belongsToProtocol(anInvocation.selector, self.protocol, &required, &instance);
+        NSAssert(sanity && instance, @"Selector %@ is not actually part of protocol %@?!", NSStringFromSelector(anInvocation.selector), NSStringFromProtocol(self.protocol));
+#endif
+        
+        // This loops over .observers.allObjects to retain the members of the collection for the duration of the loop. Intentional mutations to the collection were being avoided, but object deallocation was unpredictably mutating the collection during fast enumeration.
+        // In fact, there should probably be a radar about using fast enumeration on weak collections…
+        for (id obj in self.observers.allObjects) {
+            if ([obj respondsToSelector:anInvocation.selector] || required) {
+                [anInvocation invokeWithTarget:obj];
+            }
+        }
     }
 }
 
